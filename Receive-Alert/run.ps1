cd83039071ece527e4daadebf38d450b3f3d9341
lwhitelock <79275328+lwhitelock@users.noreply.github.com>
2022-04-06 18:29:02 +0100

    Fix body return

--- Receive-Alert/run.ps1
+++ Receive-Alert/run.ps1
@@ -811,5 +811,5 @@ if ($Alert) {
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode = [HttpStatusCode]::OK
-        Body       = $body
+        Body       = ''
     })
